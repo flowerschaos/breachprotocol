ba7828a74f2df91edac93d176cc4978bd82d3334
flowerschaos <cfactionbackup@gmail.com>
2024-11-12 12:54:15 -0500

    beginning

--- game.py
+++ game.py
@@ -7,7 +7,24 @@ def print_dramatic_text(text: str, delay=0.1):
     print()
 
 if __name__ == '__main__':
-    print('Bludgeons & Flagons')
+    print_dramatic_text('Geiger-Based Metronome')
+    print_dramatic_text('A mildly radioactive adventure set in the Fallout universe')
+
+    strengthNum = 6
+    per = 6
+    end = 6
+    cha = 6
+    intel = 6
+    agi = 5
+    lck = 5
 
     # collecting user input
     name = input('Name: ')
+    strength = ('S - '+strengthNum)
+    perception = ('P - '+per)
+    endurance = ('E - '+end)
+    charisma = ('C - '+cha)
+    intelligence = ('I - '+intel)
+    agility = ('A - '+agi)
+    luck = ('L - '+lck)
+    
